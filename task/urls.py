9fab3613541912c33dbf036687bd1821eb4b50c9
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-06-29 22:31:36 +0300

    Added TaskDeleteView in urls

--- task/urls.py
+++ task/urls.py
@@ -8,6 +8,7 @@ from task.views import (
     TagDeleteView,
     TaskCreateView,
     TaskUpdateView,
+    TaskDeleteView,
 )
 
 urlpatterns = [
@@ -18,6 +19,7 @@ urlpatterns = [
     path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 ]
 
 app_name = "tasks"
