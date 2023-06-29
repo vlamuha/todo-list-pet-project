a0a5e3a7681cad3bbce5b58014817bd3e1849b91
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-06-29 22:08:56 +0300

    Fixed tasks to task

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("tasks.urls", namespace="tasks")),
+    path("", include("task.urls", namespace="tasks")),
 ]
