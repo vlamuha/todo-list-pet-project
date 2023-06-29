2c4c5bb350831bfffe0205b70563948f8b6d9349
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-06-29 22:24:00 +0300

    Added TagCreateView in urls

--- task/urls.py
+++ task/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from task.views import IndexView, TagListView
+from task.views import (
+    IndexView,
+    TagListView,
+    TagCreateView
+)
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
 ]
 
 app_name = "tasks"
