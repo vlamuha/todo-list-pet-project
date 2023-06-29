135cd9e8af0493fdc7f9787ae931022d916dfa31
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-06-29 22:26:35 +0300

    Added TagUpdateView in urls

--- task/urls.py
+++ task/urls.py
@@ -3,13 +3,15 @@ from django.urls import path
 from task.views import (
     IndexView,
     TagListView,
-    TagCreateView
+    TagCreateView,
+    TagUpdateView,
 )
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
 ]
 
 app_name = "tasks"
