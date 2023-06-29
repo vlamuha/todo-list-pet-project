edb07556b8f0f73af54e64d755f21311b3a29891
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-06-29 22:22:12 +0300

    Added TagListView in urls

--- task/urls.py
+++ task/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from task.views import IndexView
+from task.views import IndexView, TagListView
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
 ]
 
 app_name = "tasks"
