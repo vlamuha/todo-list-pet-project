a3c10ea2d3c346665c954e36c2fa5b49d84cc3c8
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-06-29 22:27:51 +0300

    Added TagDeleteView in urls

--- task/urls.py
+++ task/urls.py
@@ -5,6 +5,7 @@ from task.views import (
     TagListView,
     TagCreateView,
     TagUpdateView,
+    TagDeleteView,
 )
 
 urlpatterns = [
@@ -12,6 +13,7 @@ urlpatterns = [
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
     path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
 
 app_name = "tasks"
